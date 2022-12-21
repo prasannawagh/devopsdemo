cb6c14762b3c8c9d306944be2180f0de18410783
prasannawagh <prasanna.wagh@gmail.com>
2022-12-21 13:12:02 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -4,7 +4,7 @@ import os
 
 data = pd.Series([1,2,3])
 
-file_path = sys.path.append(os.environ['WORKSPACE']) + "/config/mapping/map1.csv"
+file_path = str(os.environ['WORKSPACE']) + "/config/mapping/map1.csv"
 df = pd.read_csv(file_path,sep=',')
 
 print(df)
